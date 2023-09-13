fab2d97fc0a6016034661f357480f1a33d4414d4
caoyin2 <129859365+caoyin2@users.noreply.github.com>
2023-09-13 09:23:16 +0800

    Update core.js

--- web/js/bing/chat/core.js
+++ web/js/bing/chat/core.js
@@ -10,13 +10,13 @@
 }
 )(_w.onload, _w.si_PP);
 _w.rms.js(
-  { 'A:rms:answers:Shared:BingCore.Bundle': '\/rp\/Pki1-YEXD6vos5MiDyyAeDq7sgs.br.js' },
-  { 'A:rms:answers:Web:SydneyFSCHelper': '\/rp\/y39Jb1_TQi2VlOQ95KRdAFcXZpQ.br.js' },
-  { 'A:rms:answers:VisualSystem:ConversationScope': '\/rp\/SirQHI5m5KAIhKXh7C8whoEm83U.br.js' },
-  { 'A:rms:answers:CodexBundle:cib-bundle': '\/rp\/ogCZG2k18rN-VP2q1_haam1OuGE.br.js' },
+  { 'A:rms:answers:Shared:BingCore.Bundle': '/rp/Pki1-YEXD6vos5MiDyyAeDq7sgs.br.js' },
+  { 'A:rms:answers:Web:SydneyFSCHelper': '/rp/y39Jb1_TQi2VlOQ95KRdAFcXZpQ.br.js' },
+  { 'A:rms:answers:VisualSystem:ConversationScope': '/rp/SirQHI5m5KAIhKXh7C8whoEm83U.br.js' },
+  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/ogCZG2k18rN-VP2q1_haam1OuGE.br.js' },
   { 'A:rms:answers:SharedStaticAssets:speech-sdk': '/rp/6slp3E-BqFf904Cz6cCWPY1bh9E.br.js' },
   { 'A:rms:answers:Web:SydneyWelcomeScreenBase':'/rp/tAJNECpUpSdBSxOEHv5jZ0nF93I.br.js' },
   { 'A:rms:answers:Web:SydneyWelcomeScreen':'/rp/H-Z1_dXpX8YW6Di4p6iwWmTVvMs.br.js' },
-  { 'A:rms:answers:Web:SydneyFullScreenConv': '\/rp\/oEGrfXLM8FHyB9jytbzp_nHvJyQ.br.js' },
+  { 'A:rms:answers:Web:SydneyFullScreenConv': '/rp/oEGrfXLM8FHyB9jytbzp_nHvJyQ.br.js' },
   { 'A:rms:answers:SharedStaticAssets:docx': '/rp/InEGSXbH2NVAGuGIoVebD7xJ1ik.br.js' },
 );
